fc2220506653b96cd15f5f15aca48446564ae8e5
Dont Knew <israfil123.sa@gmail.com>
2023-07-08 10:35:55 +0530

    updated

--- src/Pages/Element/SeoTag.js
+++ src/Pages/Element/SeoTag.js
@@ -1,16 +1,67 @@
-import { Helmet } from 'react-helmet';
+import React, { useEffect } from 'react';
 
-export default function SeoTag({ tag }) {
-  const currentUrl = window.location.href;
+function SeoTags({ title, metaDescription, metaKeywords }) {
+  const canonicalUrl = window.location.href;
+  useEffect(() => {
+    // Update the title
+    document.title = title;
 
-  return (
-    <>
-      <Helmet>
-        <title>{tag.title}</title>
-        <meta name="description" content={tag.description} />
-        <meta name="keywords" content={tag.keywords} />
-        <link rel="canonical" href={currentUrl} />
-      </Helmet>
-    </>
-  );
+    // Update the meta description
+    const metaDescriptionTag = document.querySelector('meta[name="description"]');
+    if (metaDescriptionTag) {
+      metaDescriptionTag.setAttribute('content', metaDescription);
+    } else {
+      const newMetaTag = document.createElement('meta');
+      newMetaTag.setAttribute('name', 'description');
+      newMetaTag.setAttribute('content', metaDescription);
+      document.head.appendChild(newMetaTag);
+    }
+
+    // Update the meta keywords
+    const metaKeywordsTag = document.querySelector('meta[name="keywords"]');
+    if (metaKeywordsTag) {
+      metaKeywordsTag.setAttribute('content', metaKeywords);
+    } else {
+      const newMetaTag = document.createElement('meta');
+      newMetaTag.setAttribute('name', 'keywords');
+      newMetaTag.setAttribute('content', metaKeywords);
+      document.head.appendChild(newMetaTag);
+    }
+
+    // Update the canonical URL
+    const canonicalTag = document.querySelector('link[rel="canonical"]');
+    if (canonicalTag) {
+      canonicalTag.setAttribute('href', canonicalUrl);
+    } else {
+      const newCanonicalTag = document.createElement('link');
+      newCanonicalTag.setAttribute('rel', 'canonical');
+      newCanonicalTag.setAttribute('href', canonicalUrl);
+      document.head.appendChild(newCanonicalTag);
+    }
+
+    // Clean up function
+    return () => {
+      // Remove the meta description
+      const metaDescriptionTag = document.querySelector('meta[name="description"]');
+      if (metaDescriptionTag) {
+        metaDescriptionTag.remove();
+      }
+
+      // Remove the meta keywords
+      const metaKeywordsTag = document.querySelector('meta[name="keywords"]');
+      if (metaKeywordsTag) {
+        metaKeywordsTag.remove();
+      }
+
+      // Remove the canonical URL
+      const canonicalTag = document.querySelector('link[rel="canonical"]');
+      if (canonicalTag) {
+        canonicalTag.remove();
+      }
+    };
+  }, [title, metaDescription, metaKeywords, canonicalUrl]);
+
+  return null; // This component doesn't render anything
 }
+
+export default SeoTags;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,16 @@
+import SeoTags from "./Element/SeoTag";
 import Footer from "./Footer";
 import Header from "./Header";
 function Contact() {
   return (
     <>
+    <SeoTags title="Home" metaDescription="Home Page Description" metaKeywords="Keywords Render" />
     <Header />
     <div id="carouselExample" className="carousel slide">
     <div className="carousel-inner">
       <div className="carousel-item active">
         <img src="image.jpg" className="d-block w-100" height={400} alt="..." />
       </div>
-      
       <div className="carousel-item">
         <img src="image2.jpg" className="d-block w-100" alt="..." height={400} />
       </div>
